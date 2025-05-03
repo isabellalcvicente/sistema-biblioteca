47ab354ddbd47df943f24e32ddb02c2aef29bf6c
isabellalcvicente <isabellacadastros16@gmail.com>
2025-05-02 21:05:04 -0300

    Create

---
+++ ListaEncadeada/lista_dupla.h
@@ -0,0 +1,46 @@
+#ifndef LISTA_DUPLA_H
+#define LISTA_DUPLA_H
+
+#define MAX_TITULO 100
+#define MAX_AUTOR 100
+#define MAX_ISBN 14
+#define MAX_USUARIO 100
+#define MAX_DATA 11
+
+typedef enum {
+    DISPONIVEL,
+    EMPRESTADO
+} StatusLivro;
+
+typedef struct {
+    char titulo[MAX_TITULO];
+    char autor[MAX_AUTOR];
+    int ano;
+    char isbn[MAX_ISBN];
+    StatusLivro status;
+    char usuario[MAX_USUARIO];
+    char dataEmprestimo[MAX_DATA];
+} Livro;
+
+typedef struct NoLivro {
+    Livro livro;
+    struct NoLivro* proximo;
+    struct NoLivro* anterior;
+} NoLivro;
+
+typedef struct {
+    NoLivro* cabeca;
+    NoLivro* cauda;
+    int totalLivros;
+} Biblioteca;
+
+void inicializarBiblioteca(Biblioteca* b);
+int cadastrarLivro(Biblioteca* b, char* titulo, char* autor, int ano, char* isbn);
+void listarTodosLivros(Biblioteca* b);
+void listarTodosLivrosReverso(Biblioteca* b);
+int emprestarLivro(Biblioteca* b, char* isbn, char* usuario, char* data);
+int devolverLivro(Biblioteca* b, char* isbn);
+NoLivro* consultarLivroPorISBN(Biblioteca* b, char* isbn);
+void destruirBiblioteca(Biblioteca* b);
+
+#endif
